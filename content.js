a95ece5dbd88db983654ae06c3244ac22bca93fd
a22wong <a22wong.zm@gmail.com>
2017-10-14 17:53:00 -0400

    more deets

--- content.js
+++ content.js
@@ -1,21 +1,5 @@
 var titles = document.getElementsByClassName('jobtitle');
 
-var jobs = {
-	"Customer Service Representative":"65.0",
-	"Visa Customer Service Officer":"1.0",
-	"Credit Card Customer Service Officer":"1.0",
-	"Business Analyst":"11.0",
-	"Community Projects Coordinator":"49.0",
-	"Audio Visual Technician":"36.0",
-	"Casual Frontline Staffing Customer Service Representative":"65.0",
-	"Financial Services Representative":"65.0",
-	"Overnight Financial Telephone Representative":"",
-	"TD MBNA Customer Service Officer":"1.0",
-	"Financial Services Representative (Part-time Position)":"46.0",
-	"Financial Services Representative Part-time":"46.0",
-	"Customer Service Representative - First Choice Mobile Pool":""
-};
-
 
 var job_general = {
 	"Default":"35.0",
@@ -26,6 +10,8 @@ var job_general = {
 	"Coordinator":"49.0",
 	"Technician":"36.0",
 	"Planner":"13.0",
+	"Accountant":"75.0"
+	"Specialist":"25.0"
 };
 
 // use regex to make more generalizable
