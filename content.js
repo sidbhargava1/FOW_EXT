f24f0b3e3ed7b3bb5a9ee050acc9b9ee1c992989
a22wong <a22wong.zm@gmail.com>
2017-10-14 17:28:57 -0400

    extention searches all, not just td

--- content.js
+++ content.js
@@ -20,11 +20,12 @@ var jobs = {
 var job_general = {
 	"Default":"35.0",
 	"Representative":"65.0",
+	"Financial * Representative":"64.0",
 	"Officer":"1.0",
 	"Analyst":"11.0",
 	"Coordinator":"49.0",
 	"Technician":"36.0",
-	"Planner":"13.0"
+	"Planner":"13.0",
 };
 
 // use regex to make more generalizable
