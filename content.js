a980dfa6a46ec7f3e64d2fa5eb92abac6637f6c9
a22wong <a22wong.zm@gmail.com>
2017-10-14 18:07:19 -0400

    more deets

--- content.js
+++ content.js
@@ -4,14 +4,22 @@ var titles = document.getElementsByClassName('jobtitle');
 var job_general = {
 	"Default":"35.0",
 	"Representative":"65.0",
-	"Financial * Representative":"64.0",
 	"Officer":"1.0",
 	"Analyst":"11.0",
 	"Coordinator":"49.0",
 	"Technician":"36.0",
 	"Planner":"13.0",
-	"Accountant":"75.0"
-	"Specialist":"25.0"
+	"Accountant":"75.0",
+	"Specialist":"25.0",
+	"Developer":"40.0",
+	"Manager":"24.0",
+	"Agent":"46.0",
+	"Designer":"25.0",
+	"Associate":"33.0",
+	"Administrator":"48.0",
+	"Specialist":"37.0",
+	"Clerk":"86.0"
+
 };
 
 // use regex to make more generalizable
