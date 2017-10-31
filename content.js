ff1fe431ee0460f189ba6f7b1b79f9d66ed61784
Toby <toby.james.thomas@gmail.com>
2017-10-30 23:53:40 -0400

    add least susceptible skills, add skill percentages, add skill percent toggle, comment code, move data to data.js, change colours

--- content.js
+++ content.js
@@ -1,84 +1,75 @@
 var titles = document.getElementsByClassName('jobtitle');
 
-
-var job_general = {
-	"Default":{percent:"35.0", skills: ["Default", "Default", "Default", "Default"]},
-	"Representative":{percent:"65.0", skills: ["Representative", "Representative", "Representative", "Representative"]},
-	"Officer":{percent:"1.0", skills: ["Officer", "Officer", "Officer", "Officer"]},
-	"Analyst":{percent:"11.0", skills: ["Analyst", "Analyst", "Analyst", "Analyst"]},
-	"Coordinator":{percent:"49.0", skills: ["Coordinator", "Coordinator", "Coordinator", "Coordinator"]},
-	"Technician":{percent:"36.0", skills: ["Technician", "Technician", "Technician", "Technician"]},
-	"Planner":{percent:"13.0", skills: ["Planner", "Planner", "Planner", "Planner"]},
-	"Accountant":{percent:"75.0", skills: ["Accountant", "Accountant", "Accountant", "Accountant"]},
-	"Specialist":{percent:"25.0", skills: ["Specialist", "Specialist", "Specialist", "Specialist"]},
-	"Developer":{percent:"40.0", skills: ["Developer", "Developer", "Developer", "Developer"]},
-	"Manager":{percent:"24.0", skills: ["Manager", "Manager", "Manager", "Manager"]},
-	"Agent":{percent:"46.0", skills: ["Agent", "Agent", "Agent", "Agent"]},
-	"Designer":{percent:"25.0", skills: ["Designer", "Designer", "Designer", "Designer"]},
-	"Associate":{percent:"33.0", skills: ["Associate", "Associate", "Associate", "Associate"]},
-	"Administrator":{percent:"48.0", skills: ["Administrator", "Administrator", "Administrator", "Administrator"]},
-	"Specialist":{percent:"37.0", skills: ["Specialist", "Specialist", "Specialist", "Specialist"]},
-	"Clerk":{percent:"86.0", skills: ["Clerk", "Clerk", "Clerk", "Clerk"]}
-};
-
-// use regex to make more generalizable
+// SHOW SKILL PERCENTAGES
+var showSkillSusceptibility = true;
 
 for (var i = 0, l = titles.length; i < l; i++) {
-	var jobType = job_general['Default'];
+
+	// Match closest job type to posting
+	var jobType = jobTypes['Default'];
 	title = titles[i].innerText;
-	for(job in job_general){
-		var re = new RegExp(job);
+	for(job in jobTypes){
+		var re = new RegExp(job); // use regex to make more generalizable
 		if(re.test(title) == true){
-			jobType = job_general[job];
+			jobType = jobTypes[job];
 		}
 	}
 
-	var prob = jobType.percent;
-	var skills = jobType.skills;
+	// Get data from jobType
+	var prob = jobType.percent; // susceptibility of job type
+	var mostSuscept = jobType.mostSuscept; // skills most susceptible to automation
+	var leastSuscept = jobType.leastSuscept; // skills least susceptible to automation
 
+	// Create div element for automation line and inject into right spot
 	var div = document.createElement("div");
 	sponsor = titles[i].classList.contains("turnstileLink");
 	div.className += " sjcl automatable automatable-" + i;
 	sibling = titles[i].nextSibling;
-
 	if (sponsor == true) {
 		sibling = titles[i].parentElement.getElementsByClassName('sjcl')[0];
 	}
-
 	var br = document.createElement("br");
 	titles[i].parentElement.insertBefore(div,sibling);
 	div.parentElement.insertBefore(br,sibling);
 
+	// Create content for Tipper popup
 	var content = "<p>This job is " + prob + "% automatable.</p>";
+
+	// List most susceptible skills
 	content += "<p>These are the skills most susceptible to automation:</p>";
 	content += "<ul>";
-	for (var j = 0; j < skills.length; j++) {
-		content += "<li><a href='#'>" + skills[j] + "</a></li>";
+	for (var j = 0; j < mostSuscept.length; j++) {
+		content += "<li><a href='#'>" + mostSuscept[j].skill;
+		content += showSkillSusceptibility ? " – " + mostSuscept[j].percent + "%" : "";
+		content += "</a></li>";
 	}
 	content += "</ul>";
 
-	// content += "<p>Here are related jobs that are less automatable:</p>";
-	// content += "<ul>";
-	// var relatedJobs = ["Job1", "Job2", "Job3", "Job4"];
-	// for (var j = 0; j < relatedJobs.length; j++) {
-	// 	content += "<li><a href='#'>" + relatedJobs[j] + "</a></li>";
-	// }
-	// content += "</ul>";
+	// List least susceptible skills
+	content += "<p>These are the skills least susceptible to automation:</p>";
+	content += "<ul>";
+	for (var j = 0; j < leastSuscept.length; j++) {
+		content += "<li><a href='#'>" + leastSuscept[j].skill;
+		content += showSkillSusceptibility ? " – " + leastSuscept[j].percent + "%" : "";
+		content += "</a></li>";
+	}
+	content += "</ul>";
 
+	// Set div attributes
 	div.setAttribute("data-title", title);
 	div.setAttribute("data-content", content);
 	div.innerText = prob + "% of this job is automatable.";
 
+	// Set the colour of automation line
 	var color = 'black';
-
 	if (prob <= 30) {
-		color = 'limegreen';
+		color = '#4ccc51';
 	} else if (prob > 60) {
-		color = 'red';
+		color = '#f44336';
 	}
-
 	div.style.color = color;
 
+	// Create Tipper pop up for div element
 	var divClass = ".automatable-" + i;
 	Tipped.create(divClass, function(element) {
       return {
@@ -90,7 +81,7 @@ for (var i = 0, l = titles.length; i < l; i++) {
 		size: 'large',
 		title: true,
 		close: true,
-		hideOn: false,
+		//hideOn: false, // Uncomment to disable hide on hover off
 		hideOthers: true,
 		maxWidth: 400
 	});
---
+++ data.js
@@ -0,0 +1,72 @@
+var jobTypes = {
+	"Default":{
+		percent:"35.0",
+		mostSuscept: [{skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}],
+		leastSuscept: [{skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}]
+	},
+	"Representative":{
+		percent:"65.0",
+		mostSuscept: [{skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}],
+		leastSuscept: [{skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}]
+	},
+	"Officer":{
+		percent:"1.0",
+		mostSuscept: [{skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}],
+		leastSuscept: [{skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}]},
+	"Analyst":{
+		percent:"11.0",
+		mostSuscept: [{skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}],
+		leastSuscept: [{skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}]},
+	"Coordinator":{
+		percent:"49.0",
+		mostSuscept: [{skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}],
+		leastSuscept: [{skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}]},
+	"Technician":{
+		percent:"36.0",
+		mostSuscept: [{skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}],
+		leastSuscept: [{skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}]},
+	"Planner":{
+		percent:"13.0",
+		mostSuscept: [{skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}],
+		leastSuscept: [{skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}]},
+	"Accountant":{
+		percent:"75.0",
+		mostSuscept: [{skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}],
+		leastSuscept: [{skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}]},
+	"Specialist":{
+		percent:"25.0",
+		mostSuscept: [{skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}],
+		leastSuscept: [{skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}]},
+	"Developer":{
+		percent:"40.0",
+		mostSuscept: [{skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}],
+		leastSuscept: [{skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}]},
+	"Manager":{
+		percent:"24.0",
+		mostSuscept: [{skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}],
+		leastSuscept: [{skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}]},
+	"Agent":{
+		percent:"46.0",
+		mostSuscept: [{skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}],
+		leastSuscept: [{skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}]},
+	"Designer":{
+		percent:"25.0",
+		mostSuscept: [{skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}],
+		leastSuscept: [{skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}]},
+	"Associate":{
+		percent:"33.0",
+		mostSuscept: [{skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}],
+		leastSuscept: [{skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}]},
+	"Administrator":{
+		percent:"48.0",
+		mostSuscept: [{skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}],
+		leastSuscept: [{skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}]},
+	"Specialist":{
+		percent:"37.0",
+		mostSuscept: [{skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}],
+		leastSuscept: [{skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}]},
+	"Clerk":{
+		percent:"86.0",
+		mostSuscept: [{skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}, {skill:"Most1",percent:"50.0"}],
+		leastSuscept: [{skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}, {skill:"Least1",percent:"50.0"}]}
+};
