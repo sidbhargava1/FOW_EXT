a7be1ac6b5a326950290897d0f72601d2bb7f074
Toby <toby.james.thomas@gmail.com>
2017-11-02 09:55:49 -0400

    remove errors from data.js, change layout and design of Tipper pop up

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 var titles = document.getElementsByClassName('jobtitle');
 
 // SHOW SKILL PERCENTAGES
-var showSkillSusceptibility = true;
+var showSkillSusceptibility = false;
 
 for (var i = 0, l = titles.length; i < l; i++) {
 
@@ -32,34 +32,6 @@ for (var i = 0, l = titles.length; i < l; i++) {
 	titles[i].parentElement.insertBefore(div,sibling);
 	div.parentElement.insertBefore(br,sibling);
 
-	// Create content for Tipper popup
-	var content = "<p>This job is " + prob + "% automatable.</p>";
-
-	// List most susceptible skills
-	content += "<p>These are the skills most susceptible to automation:</p>";
-	content += "<ul>";
-	for (var j = 0; j < mostSuscept.length; j++) {
-		content += "<li><a href='#'>" + mostSuscept[j].skill;
-		content += showSkillSusceptibility ? " – " + mostSuscept[j].percent + "%" : "";
-		content += "</a></li>";
-	}
-	content += "</ul>";
-
-	// List least susceptible skills
-	content += "<p>These are the skills least susceptible to automation:</p>";
-	content += "<ul>";
-	for (var j = 0; j < leastSuscept.length; j++) {
-		content += "<li><a href='#'>" + leastSuscept[j].skill;
-		content += showSkillSusceptibility ? " – " + leastSuscept[j].percent + "%" : "";
-		content += "</a></li>";
-	}
-	content += "</ul>";
-
-	// Set div attributes
-	div.setAttribute("data-title", title);
-	div.setAttribute("data-content", content);
-	div.innerText = prob + "% of this job is automatable.";
-
 	// Set the colour of automation line
 	var color = 'black';
 	if (prob <= 30) {
@@ -67,6 +39,33 @@ for (var i = 0, l = titles.length; i < l; i++) {
 	} else if (prob > 60) {
 		color = '#f44336';
 	}
+
+	// Create content for Tipper popup
+	var content = "<p style='color:" + color + "; font-weight:bold;'>This job is " + prob + "% automatable.</p>";
+
+	// Create table listing most and least susceptible skills
+	content += "<p>These are the skills most susceptible to automation:</p>";
+	content += "<table class='susceptible-skills-table'><tbody>";
+	content += "<tr class='header'><th>Most Susceptible</th><th>Least Susceptible</th></tr>";
+	content += "<tr class='body'><td><ul>";
+	for (var j = 0; j < mostSuscept.length; j++) {
+		content += "<li><a href='#'>" + mostSuscept[j].skill;
+		content += showSkillSusceptibility ? " – " + mostSuscept[j].percent + "%" : "";
+		content += "</a></li>";
+	}
+	content += "</ul></td><td><ul>";
+	for (var j = 0; j < leastSuscept.length; j++) {
+		content += "<li><a href='#'>" + leastSuscept[j].skill;
+		content += showSkillSusceptibility ? " – " + leastSuscept[j].percent + "%" : "";
+		content += "</a></li>";
+	}
+	content += "</ul></td></tr>";
+	content += "</tbody></table>";
+
+	// Set div attributes
+	div.setAttribute("data-title", title);
+	div.setAttribute("data-content", content);
+	div.innerText = prob + "% of this job is automatable.";
 	div.style.color = color;
 
 	// Create Tipper pop up for div element
@@ -83,6 +82,6 @@ for (var i = 0, l = titles.length; i < l; i++) {
 		close: true,
 		//hideOn: false, // Uncomment to disable hide on hover off
 		hideOthers: true,
-		maxWidth: 400
+		maxWidth: 325
 	});
 }
